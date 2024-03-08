993fb463e08c18f5434a7dac0ca7d4da3a72fcdc
정준일 <dala1207@naver.com>
2024-03-08 12:32:38 +0900

    안녕하세요!

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -7,8 +7,8 @@ public class Hello{
 	    public static void main(String[] args) {
 	    	String str = "1990-12-07"; 
 	    	
-	    	String str1 = str.replaceAll("-", "").substring(2); 
-	    	
+	    	String str1 = str.replaceAll("-", "").substring(2);
+			System.out.println("안녕하세요!");
 	    	System.out.println(str1);
 	    }
 
